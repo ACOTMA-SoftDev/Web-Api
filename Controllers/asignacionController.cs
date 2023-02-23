0665b89b88c1080f115c95c5ce21f79d75beac91
BrendaCarrera <brenda.huitzil.26@gmail.com>
2023-02-23 13:45:04 -0600

    creation of the update method of asignacion

---
+++ Controllers/asignacionController.cs
@@ -0,0 +1,23 @@
+﻿using Acotma_API.Models_DB.EntityModels;
+using Acotma_API.serviciosModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Acotma_API.Controllers
+{
+    public class asignacionController : ApiController
+    {
+        asignacionServicio serviceAsigna = new asignacionServicio();
+
+        [HttpPost]
+
+        public bool updateAsigancion(asignacionEntity uAsignacion)
+        {
+            return serviceAsigna.UpdateAsignacion(uAsignacion);
+        }
+    }
+}
---
+++ serviciosModels/asignacionServicio.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using Acotma_API.Models_DB.EntityModels;
+using Acotma_API.Models_DB;
+
+namespace Acotma_API.serviciosModels
+{
+
+   public class asignacionServicio
+    {
+        ACOTMADBEntities DB = new ACOTMADBEntities();
+        public bool UpdateAsignacion(asignacionEntity asigna)
+        {
+            bool response = false;
+            try
+            {
+                asignacion newAsignacion = (new asignacion
+                {
+                    idAsignacion = asigna.idAsignacion
+                });
+                asignacion oldAsigancion = DB.asignacion.FirstOrDefault(a => a.idAsignacion == asigna.idAsignacion);
+                oldAsigancion.idAsignacion = newAsignacion.idAsignacion;
+
+                DB.SaveChanges();
+                response = true;
+
+            }
+
+        catch(Exception e)
+            {
+                String mensaje = e.Message;
+            }
+            return response;
+        }
+        
+
+
+    }
+
+    
+
+}
